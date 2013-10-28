cfc0326ce51c5e1d58f86f30d7edfde640af3d8e
Eric Rinkus <ejrinkus@mtu.edu>
2013-10-28 04:10:03 -0400

    Created constants file, added todos

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Netris
 Netris is the future.  A brotastic retro-future where tetris may now be played over a network (hopefully).
 
 WARNING:  This game may cause massive amounts of facemelting.  And this readme may be a bit of an exaggeration...
+
+Version History
+===============
+0.1 Basic game - no menu, score, or round counter, one speed.  Only has basic controls (shift left/right, drop fast, drop instant, and rotate clockwise), and shows the next piece.  Completed lines disappear, blocks properly stack, game exits when top boundary is breached.
--- netris.py
+++ netris.py
@@ -1,21 +1,29 @@
 __author__ = 'Eric'
 
-
-import pygame, sys
-from pygame.locals import *
-
-WIDTH = 800
-HEIGHT = 600
+from header import *
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((800,600))
+DISPLAYSURF = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Netris v.0.1')
+# TODO: Load the block spritesheet (use the sprite module from pygame)
+
+# TODO: Set up the playing board
+# TODO: Set up the next piece box
 
 while True: # main game loop
+    #TODO: Decide if next piece should spawn
+    #TODO: Determine next piece (and display in box)
+
     for event in pygame.event.get(): # event handler
-
-
+        #TODO: Up arrow rotates
+        #TODO: Left/right arrows shift
+        #TODO: Down arrow drops faster
+        #TODO: Space instant drops
+        #TODO: P or ESC pauses
         if event.type == QUIT: # exits the game safely
             pygame.quit()
             sys.exit()
+
+    #TODO: Determine if piece should stop moving
+    #TODO: Determine if top boundary is breached
     pygame.display.update()
