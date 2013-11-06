691147fc612e03cbc8e6109f93b7bf2e602af664
Eric Rinkus <ejrinkus@mtu.edu>
2013-11-06 15:36:45 -0500

    Hold functionality implemented and working

--- netris.py
+++ netris.py
@@ -22,6 +22,15 @@ def preparePiece(p):
             if change != 'E':
                 board.changeCell(i+offi,j+offj,change,sprites.get(change).image)
 
+def swapPieces(a,b):
+        a.id, b.id = b.id, a.id
+        a.width, b.width = b.width, a.width
+        a.height, b.height = b.height, a.height
+        a.matrix, b.matrix = b.matrix, a.matrix
+        a.state, b.state = b.state, a.state
+        a.kicks, b.kicks = b.kicks, a.kicks
+
+
 # Playing board
 board = Grid((SCREEN_W/2 - BOX_SIZE*5, BOX_SIZE/5), 10, 22)
 board.drawChanges(disp)
@@ -29,6 +38,10 @@ board.drawChanges(disp)
 # Next piece box
 next_box = Grid((board.location[0] - BOX_SIZE*6, BOX_SIZE/5 + BOX_SIZE*2), 5, 5)
 next_box.drawChanges(disp)
+
+# Hold box
+hold_box = Grid((board.location[0] + BOX_SIZE*11, BOX_SIZE/5 + BOX_SIZE*2), 5, 5)
+hold_box.drawChanges(disp)
 
 # Sprite array
 sheet = pygame.image.load(BLOCK_FILE).convert()
@@ -43,6 +56,8 @@ sprites = {'I' : Block(sheet,'I'),
 # Flags and other necessary variables
 paused = False
 spawn = True
+hold = False
+hold_flag = False
 pygame.time.set_timer(INPUT_TIMER, 50)
 pygame.time.set_timer(DROP_TIMER, 1000)
 u_hold = d_hold = l_hold = r_hold = 3
@@ -52,13 +67,34 @@ order = ['I','J','L','O','S','T','Z']
 shuffle(order)
 count = 0
 nextpiece = Tetromino(order[count])
+heldpiece = Tetromino()
 
 # Main game loop
 while True:
 
+    # Switch out the held piece
+    if hold:
+        hold = hold_flag = False
+        clearPiece(activepiece)
+        swapPieces(activepiece,heldpiece)
+        # Draw held piece
+        hold_box.clear()
+        for i,row in enumerate(heldpiece.matrix):
+            for j,block in enumerate(row):
+                if block != 'E':
+                    hold_box.changeCell(i+1,j+1,block,sprites.get(block).image)
+                else:
+                    hold_box.changeCell(i+1,j+1,block,None)
+        # Prepare active piece for drawing (or spawn new piece if there was no held piece)
+        if activepiece.id != 'E':
+            preparePiece(activepiece)
+        else:
+            spawn = True
+
     # Spawn a new piece, and determine the next piece
     if spawn:
         spawn = False
+        hold_flag = True
         activepiece = nextpiece
         count += 1
         if count > 6:
@@ -68,14 +104,13 @@ while True:
         next_box.clear()
         # Draw next piece
         for i,row in enumerate(nextpiece.matrix):
-            for j,block in enumerate(nextpiece.matrix[i]):
+            for j,block in enumerate(row):
                 if block != 'E':
                     next_box.changeCell(i+1,j+1,block,sprites.get(block).image)
                 else:
                     next_box.changeCell(i+1,j+1,block,None)
         # Prepare active piece for drawing
         preparePiece(activepiece)
-
 
     # Event handler
     for event in pygame.event.get():
@@ -94,20 +129,20 @@ while True:
                 if event.key == K_DOWN:
                     down = d_first = True
                     d_hold = 3
-                if event.key == K_LCTRL:
+                if event.key == K_LCTRL and activepiece.id != 'O':
                     for i, row in enumerate(activepiece.matrix):
                         for j, block in enumerate(row):
                             if block != 'E':
                                 board.changeCell(i+activepiece.coord[1], j+activepiece.coord[0], 'E', None)
                     board.validRotLeft(activepiece)
-                if event.key == K_RCTRL:
+                if event.key == K_RCTRL and activepiece.id != 'O':
                     for i, row in enumerate(activepiece.matrix):
                         for j, block in enumerate(row):
                             if block != 'E':
                                 board.changeCell(i+activepiece.coord[1], j+activepiece.coord[0], 'E', None)
                     board.validRotRight(activepiece)
-                if event.key == K_SPACE:
-                    spawn = True
+                if event.key == K_SPACE and hold_flag == True:
+                    hold = True
 
             # Pause the game
             if event.key == K_p:
@@ -152,9 +187,10 @@ while True:
                     else: d_hold -= 1
 
         if event.type == DROP_TIMER:
-            clearPiece(activepiece)
-            if board.validMoveDown(activepiece) is None:
-                spawn = True
+            if not paused:
+                clearPiece(activepiece)
+                if board.validMoveDown(activepiece) is None:
+                    spawn = True
 
     # Prepare active piece for drawing
     preparePiece(activepiece)
@@ -166,5 +202,6 @@ while True:
 
     # Draw everything
     next_box.drawChanges(disp)
+    hold_box.drawChanges(disp)
     board.drawChanges(disp)
     pygame.display.update()
